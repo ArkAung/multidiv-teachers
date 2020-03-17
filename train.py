be60797c40afebcb5bc56889c5f8c608200a6baa
Arkar Aung <arica.aung@gmail.com>
2020-03-18 00:10:17 +0800

    Add acc for training loop

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ def train(epochs, device, dataloader, net, optimizer, criterion):
     for e in range(1, epochs+1): # For each epoch
         running_loss = 0
         num_datapoints = 0
+        correct = 0
         with tqdm(total=total_batches) as pbar:
             for batch_idx, data in enumerate(dataloader):  # For each batch
                 images, labels = data[0].to(device), data[1].to(device)
@@ -21,7 +22,9 @@ def train(epochs, device, dataloader, net, optimizer, criterion):
 
                 running_loss += loss.item()
                 num_datapoints += len(labels)
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
 
             mean_loss = running_loss/num_datapoints
             pbar.update(1)
-        print("Epoch: {} -- Average Loss: {}".format(e,mean_loss))
+        print("Epoch: {} -- Average Loss: {} -- Acc: {}%".format(e, mean_loss, 100*correct/num_datapoints))
